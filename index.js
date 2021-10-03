7b7dce81fc1173fdbd3714160ad89dd746e06046
Klamkaa <91829516+Klamkaa@users.noreply.github.com>
2021-10-03 19:01:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-console.log('MEMULAI BOT SILAHKAN TUNGGU SAMPAI PROSES SELESAI')
-let { spawn } = require('child_process')
-let path = require('path')
-let fs = require('fs')
-let package = require('./package.json')
-const CFonts  = require('cfonts')
-CFonts.say('WHATSAPP BOT\nKlamKaBot', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-CFonts.say(`'This WhatsApp Bot Has Been Recoded By KlamKaBot`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-
-var isRunning = false
-/**
- * Start a js file
- * @param {String} file `path/to/file`
- */
-function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
-  CFonts.say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  })
-  let p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-  })
-  p.on('message', data => {
-    console.log('[DITERIMA]', data)
-    switch (data) {
-      case 'reset':
-        p.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-  p.on('exit', code => {
-    isRunning = false
-    console.error('Exited with code:', code)
-    if (code === 0) return
-    fs.watchFile(args[0], () => {
-      fs.unwatchFile(args[0])
-      start(file)
-    })
-  })
-  // console.log(p)
+module.exports = {
+    ...require('./adapters/JSONFile.js'),
+    ...require('./adapters/JSONFileSync.js'),
+    ...require('./adapters/LocalStorage.js'),
+    ...require('./adapters/Memory.js'),
+    ...require('./adapters/MemorySync.js'),
+    ...require('./adapters/TextFile.js'),
+    ...require('./adapters/TextFileSync.js'),
+    ...require('./Low.js'),
+    ...require('./LowSync.js'),
 }
-
-start('main.js')
